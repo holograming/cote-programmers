db609865fda80ac3929d0e403e7e6bbdb75dea40
Essspressso <hologram1220@gmail.com>
2025-12-29 09:10:42 +0900

    [level 3] Title: 다단계 칫솔 판매, Time: 35.69 ms, Memory: 18.7 MB -BaekjoonHub

--- 프로그래머스/3/77486. 다단계 칫솔 판매/README.md
+++ 프로그래머스/3/77486. 다단계 칫솔 판매/README.md
@@ -1,10 +1,10 @@
 # [level 3] 다단계 칫솔 판매 - 77486 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/77486?language=cpp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/77486) 
 
 ### 성능 요약
 
-메모리: 18.6 MB, 시간: 30.99 ms
+메모리: 18.7 MB, 시간: 35.69 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 12월 28일 23:47:22
+2025년 12월 29일 09:10:41
 
 ### 문제 설명
 
--- 프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.cpp
+++ 프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.cpp
@@ -11,38 +11,32 @@ using namespace std;
 
 vector<int> solution(vector<string> enroll, vector<string> referral, 
                      vector<string> seller, vector<int> amount) {
-    int const n = enroll.size();
     
-    std::unordered_map<std::string, int> nameToIndex;
-    for (int i = 0; i < n; i++) {
-        nameToIndex[enroll[i]] = i;
+    unordered_map<string, int> idx;
+    vector<int> result(enroll.size(), 0);
+    
+    // O(N)
+    for (int i = 0; i < enroll.size(); i++) {
+        idx[enroll[i]] = i;
     }
     
-    // 누적 이익금
-    std::vector<int> answer(n, 0);
-    
-    
+    // O(M × log(금액))
     for (int i = 0; i < seller.size(); i++) {
-        string currentSeller = seller[i];
-        int profit = amount[i] * 100; // 칫솔 1개당 100원
+        int money = amount[i] * 100;
+        string cur = seller[i];
         
-    
-        while (currentSeller != "-" && profit > 0) {
-            int idx = nameToIndex[currentSeller];
-            int const toReferral = profit / 10;
-            int const toKeep = profit - toReferral;
-            answer[idx] += toKeep;
+        while (cur != "-" && money > 0) {
+            int id = idx[cur];
+            int fee = money / 10;
             
-            // 버림
-            if (toReferral < 1) {
-                break;
-            }
+            result[id] += money - fee;
             
-            // 끝까지 탐색
-            currentSeller = referral[idx];
-            profit = toReferral;
+            if (fee == 0) break;  // ⭐ 핵심 최적화
+            
+            cur = referral[id];
+            money = fee;
         }
     }
     
-    return answer;
+    return result;
 }
